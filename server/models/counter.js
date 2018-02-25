72b2f418aed1c96d8ce17c0053910abc818fed23
CavinHuang <943866180@qq.com>
2018-02-25 21:53:35 +0800

    add website model websiteType model

---
+++ server/models/counter.js
@@ -0,0 +1,44 @@
+/**
+ * 自增记录
+ * @type {[type]}
+ */
+var Schema, counterSchema, mongoose;
+
+mongoose = require('mongoose');
+
+Schema = mongoose.Schema;
+
+counterSchema = new Schema({
+  _id: {
+    type: String,
+    rquired: true,
+    index: {
+      unique: true
+    }
+  },
+  count: {
+    type: Number,
+    require: true
+  }
+});
+
+counterSchema.statics.incrementCounter = function(schemaName, callback) {
+  return this.collection.findAndModify({
+    _id: schemaName
+  }, [], {
+    $inc: {
+      count: 1
+    }
+  }, {
+    "new": true,
+    upsert: true
+  }, function(err, result) {
+    if (err) {
+      return callback(err);
+    }
+    return callback(null, result.count);
+  });
+};
+
+var couter = mongoose.model('Counter', counterSchema);
+module.exports = couter
