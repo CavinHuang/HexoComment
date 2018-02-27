c8b90c2114ce71b76fea8613c549b139b12fc3fa
CavinHuang <943866180@qq.com>
2018-02-27 20:42:31 +0800

    封装axios for vue

--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,16 @@ import router from './router'
 import store from './store'
 import '@/assets/css/common.css'
 import '@/errorLog' // error log
+import {post, fetch, patch, put} from './utils/http'
+
 Vue.config.productionTip = true
+
+// 定义全局变量
+Vue.prototype.$post = post
+Vue.prototype.$fetch = fetch
+Vue.prototype.$patch = patch
+Vue.prototype.$put = put
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
