b7867dd08ad40fc216704b8d9ac1aca106c89c7b
CavinHuang <943866180@qq.com>
2018-03-09 21:58:09 +0800

    增加外部调用js组件结构

---
+++ server/public/article/index.js
@@ -0,0 +1,44 @@
+
+/**
+ * 文章操作类
+ */
+class articleUitl {
+  constructor() {
+
+  }
+
+  /**
+   * 增加count
+   */
+  addCount () {
+
+  }
+
+  /**
+   * 获取本文的访问量
+   * @return {[type]} [description]
+   */
+  getCount () {}
+
+  /**
+   * 增加文章电赞
+   */
+  addLike() {}
+
+  /**
+   * 获取文章点赞数
+   * @return {[type]} [description]
+   */
+  getLike(){}
+
+  /**
+   * 增加评论
+   */
+  addComment() {}
+
+  /**
+   * 获取文章评论
+   * @return {[type]} [description]
+   */
+  getComment(){}
+}
