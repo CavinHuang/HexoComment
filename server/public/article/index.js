c6bf8ad9d2af71d337584c7a5aee7cc69dba0890
CavinHuang <943866180@qq.com>
2018-03-28 09:48:28 +0800

    update js util

--- server/public/article/index.js
+++ server/public/article/index.js
@@ -1,61 +1,62 @@
-
 /**
  * 文章操作类
  */
+//添加请求拦截器
+
 class articleUitl {
-  constructor () {
-    this.url = 'http://127.0.0.1:3000/api'
-  }
+	constructor() {
+		this.url = 'http://127.0.0.1:3000/api'
+		this.fly = new Fly()
+		this.fly.interceptors.request.use( ( request ) => {
+			request.baseURL = 'http://127.0.0.1:3000/api'
+			return request;
+		} )
 
-  /**
-   * 增加文章点赞或者访问量
-   */
-  add (field, callback) {
-    fly.post(this.url + '/article/' + field, {
-      title: '测试文章',
-      url: 'http://blog.zukmb.cn'
-    }).then((res) => {
-      callback && callback(res.data)
-    }).catch(e => {
-      callback && callback(e)
-    })
-  }
+		//添加响应拦截器，响应拦截器会在then/catch处理之前执行
+		this.fly.interceptors.response.use(
+			( response ) => {
+				//只将请求结果的data字段返回
+				return response.data
+			},
+			( err ) => {
+				//发生网络错误后会走到这里
+				return Promise.resolve( err )
+			}
+		)
+	}
 
-  /**
-   * 获取文章点赞数
-   * @return {[type]} [description]
-   */
-  get (field, callback) {
-    fly.get(this.url + '/article/' + field, {
-      url: 'http://blog.zukmb.cn'
-    }).then((res) => {
-      callback && callback(res.data)
-    }).catch(e => {
-      callback && callback(e)
-    })
-  }
+	/**
+	 * 增加文章点赞或者访问量
+	 */
+	add( field ) {
+		return this.fly.post( '/article/' + field, {
+			title: '测试文章',
+			url: 'http://blog.zukmb.cn'
+		} )
+	}
 
-  /**
-   * 增加评论
-   */
-  addComment (data, callback) {
-    fly.post(this.url + '/comment', data).then((res) => {
-      callback && callback(res.data)
-    }).catch(e => {
-      console.log('e', e);
-      callback && callback(e)
-    })
-  }
+	/**
+	 * 获取文章点赞数
+	 * @return {[type]} [description]
+	 */
+	get( field, callback ) {
+		return this.fly.get( '/article/' + field, {
+			url: 'http://blog.zukmb.cn'
+		} )
+	}
 
-  /**
-   * 获取文章评论
-   * @return {[type]} [description]
-   */
-  getComment (callback) {
-    fly.get(this.url + '/comment', {url: window.location.href}).then((res) => {
-      callback && callback(res.data)
-    }).catch(e => {
-      callback && callback(e)
-    })
-  }
+	/**
+	 * 增加评论
+	 */
+	addComment( data, callback ) {
+		return this.fly.post( '/comment', data )
+	}
+
+	/**
+	 * 获取文章评论
+	 * @return {[type]} [description]
+	 */
+	getComment( callback ) {
+		return this.fly.get( this.url + '/comment', { url: window.location.href } )
+	}
 }
